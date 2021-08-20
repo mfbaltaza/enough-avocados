953ef16e3a706502737704909f272835f8e325c9
Juan Araujo <araujogreydiego@gmail.com>
2021-08-20 13:45:36 -0400

    Updates API Links

--- pages/yes-or-no.tsx
+++ pages/yes-or-no.tsx
@@ -10,7 +10,7 @@ type YesOrNoApiResponse = {
 }
 
 const fetchResult = async () => {
-  const res = await fetch('https://platzi-avo.vercel.app/api/yes-or-no')
+  const res = await fetch('https://enough-avocados.vercel.app/api/yes-or-no')
   const { data }: YesOrNoApiResponse = await res.json()
 
   return data
